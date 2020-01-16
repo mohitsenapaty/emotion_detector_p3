31ab6652769d9f514ed85404f87bc003b773a0b1
root <root@localhost.localdomain>
2020-01-16 08:47:55 +0530

    Changes

--- static/js/appscript/dashboard_worker.js
+++ static/js/appscript/dashboard_worker.js
@@ -13,7 +13,7 @@ var blob = new Blob([
             let eventName = 'lecture_'+lectureid;
             console.log(eventName);
             io.socket.on(eventName, (message) => {
-                console.log(message);
+                //console.log(message);
                 postMessage(message);
             });
         }
@@ -33,6 +33,12 @@ worker.postMessage(); // Start the worker.
 
 var w;
 var blobURL = window.URL.createObjectURL(blob);
+let numStudents = 0;
+let numPoints = 0;
+let sumAvgTotal = 0.0;
+let avgTotal = 0.0;
+let avgTotalArr = [];
+let studentDataObj = {};
 //var mySocket = io.sails.connect();
 
 function startWorker()
@@ -45,8 +51,37 @@ function startWorker()
         //When the web worker posts a message, the code within the event listener is executed. The data from the web worker
         //is stored in event.data.
         w.onmessage = function (event) {
-            console.log(event); 
-            document.getElementById("result").innerHTML+=event.data.a; 
+            //console.log(event); 
+            //document.getElementById("result").innerHTML+=event.data.a; 
+            let maxPoints = 0;
+            let message = event.data;
+            studentDataObj = {};
+            numStudents = 0;
+            sumAvgTotal = 0.0;
+            _.forEach(message, (k, v)=>{
+                if (!_.isEmpty(v) && _.isNumber(k.avgAttentionCurrent)){
+                    let studentid = v;
+                    let studentData = k;
+                    numStudents += 1;
+                    sumAvgTotal += k.avgAttentionCurrent;
+                    studentDataObj[studentid] = k.avgAttentionCurrent;
+                    if (studentData.totalAttentionSum > maxPoints){
+                        maxPoints = studentData.totalAttentionSum;
+                    }
+                }
+            });
+            if (numStudents > 0){
+                avgTotal = sumAvgTotal/numStudents;
+                avgTotalArr.push(avgTotal*100);
+                //set to list
+                let displayStudentStr = ``;
+                _.each(studentDataObj, (k,v)=>{
+                    if (_.isNumber(k)){
+                        displayStudentStr += `<p>Student id: ${k} AttentionLevel: ${v} StudentGraph: <a>Click Here.</a> </p>`;
+                    }
+                });
+                document.getElementById("connectedStudents").innerHTML = displayStudentStr;
+            }
         };
         sendLectureidAndStartRead();
     }
@@ -66,3 +101,63 @@ function sendLectureidAndStartRead(){
 }
 
 //startWorker();
+window.onload = function () {
+
+    var dps = []; // dataPoints
+    var chart = new CanvasJS.Chart("chartContainer", {
+        title :{
+            text: "Dynamic Data"
+        },
+        axisY: {
+            includeZero: false
+        },      
+        data: [{
+            type: "line",
+            dataPoints: dps
+        }]
+    });
+
+    var xVal = 0;
+    var yVal = 100; 
+    var updateInterval = 1000;
+    var dataLength = 20; // number of dataPoints visible at any point
+
+    var updateChart = function () {
+
+        /*
+        count = count || 1;
+
+        for (var j = 0; j < count; j++) {
+            yVal = yVal +  Math.round(5 + Math.random() *(-5-5));
+            dps.push({
+                x: xVal,
+                y: yVal
+            });
+            xVal++;
+        }
+        */
+        if (avgTotalArr.length > 0){
+            dps.push({
+                x: xVal, 
+                y: avgTotalArr[avgTotalArr.length-1]
+            });
+            xVal += 1;
+        }
+        /*
+        let xval = 0;
+        _.forEach(avgTotalArr, ata =>{
+            xval+=1;
+            dps.push({
+                x: xval, y: ata
+            });
+        });
+        */
+
+        if (dps.length > 0)
+            chart.render();
+    };
+
+    //updateChart(dataLength);
+    setInterval(function(){updateChart()}, updateInterval);
+
+}
